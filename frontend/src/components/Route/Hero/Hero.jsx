aa20e18db42d5d82d422ce89e1592f6236b11fb1
Mohanbabu <mohan22102003@gmail.com>
2023-11-07 22:34:06 +0530

    Shopping Mall Management

---
+++ frontend/src/components/Route/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../../styles/styles";
+
+const Hero = () => {
+  return (
+    <div
+      className={`relative min-h-[70vh] 800px:min-h-[80vh] w-full bg-no-repeat ${styles.noramlFlex}`}
+      style={{
+        backgroundImage:
+          "url(https://themes.rslahmed.dev/rafcart/assets/images/banner-2.jpg)",
+      }}
+    >
+      <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
+        <h1
+          className={`text-[35px] leading-[1.2] 800px:text-[60px] text-[#3d3a3a] font-[600] capitalize`}
+        >
+          Best Collection for Your <br /> Home..
+        </h1>
+        <p className="pt-5 text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
+        The company itself is a very successful company. blessed
+          to assume? So anyone <br /> exercises with effort or pain
+          Indeed, a more severe, laudable time will result in an option {" "}
+          <br /> leave some responsibilities. They often provide no pain relief.
+        </p>
+        <Link to="/products" className="inline-block">
+            <div className={`${styles.button} mt-5`}>
+                 <span className="text-[#fff] font-[Poppins] text-[18px]">
+                    Shop Now
+                 </span>
+            </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
